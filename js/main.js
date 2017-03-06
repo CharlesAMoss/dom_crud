ecea42b29127cc9bb5d26783e0b888c098ed0e3a
CharlesAMoss <charlesamoss@yahoo.com>
2017-03-05 18:31:30 -0800

    refactor of edit button

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ const clear = document.querySelector('#clearBtn')
 const post = document.querySelector('#postBtn')
 const postList = document.querySelector('#postOut')
 
+
 const defaultOut = `<p> word count : 0 // character count : 0 </p>`
+
 
 function wordSpilt(str) {
   str = str.replace(/[.,\/#!$%\^&\*;:{}=`~()\"]/g,'')
@@ -45,9 +47,16 @@ function ProcessText(str) {
   this.charSplt = str.split('')
   this.wordCount = this.text.length === 0 ? 0 : this.words.length
   this.time = timeStamp()
+
+  this.interface = `<span class="post__timestamp" contenteditable="false">editing${this.time}</span><span class="post__edit" style="visibility: visible;" contenteditable="false">✓</span><span class="post__remove" style="visibility: visible;" contenteditable="false">x</span>`
+
   this.textInfo = () => `<p>word count : ${this.wordCount} // character count : ${this.text.length}</p>`
+
   this.textOut = () => `<li class="posts__item">${this.text}<span class="post__timestamp">${this.time}</span><span class="post__edit">✎</span><span class="post__remove">x</span></li>`
+
   this.textEdited = () => `<li class="posts__item">${this.text}<span class="post__timestamp">edited ${this.time}</span><span class="post__edit">✎</span><span class="post__remove">x</span></li>`
+
+  this.textEditing = () => `<li id="forEdit" class="posts__item" contenteditable="true">\u00A0${this.text}\u00A0\u00A0\u00A0${this.interface}</li>`
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -73,7 +82,10 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 
   postList.addEventListener('click', function(e) {
+    e.preventDefault()
+
     const toMatch = e.target.outerHTML
+
     if (e.target.className === 'posts__item') {
       e.target.lastElementChild.style.visibility = 'visible'
       e.target.lastElementChild.previousElementSibling.style.visibility = 'visible'
@@ -84,21 +96,24 @@ document.addEventListener('DOMContentLoaded', function() {
       })
 
       e.target.lastElementChild.previousElementSibling.addEventListener('click', function() {
-        e.target.setAttribute('contenteditable','true')
-        e.target.firstElementChild.setAttribute('contenteditable','false')
-        e.target.lastElementChild.setAttribute('contenteditable','false')
-        e.target.lastElementChild.previousElementSibling.setAttribute('contenteditable','false')
-        e.target.lastElementChild.previousElementSibling.innerHTML = '✓'
-        e.target.focus()
+        const info = new ProcessText(extract(e.target))
+        const postindex = postArr.indexOf(toMatch)
+        postArr.splice(postindex, 1, info.textEditing())
+        postList.innerHTML = postArr.join(``)
+        const forEdit = document.getElementById('forEdit')
+        const range = document.createRange()
+        const sel = window.getSelection()
+        range.setStart(forEdit.childNodes[0], 1)
+        range.collapse(true)
+        sel.removeAllRanges()
+        sel.addRange(range)
+        forEdit.focus()
 
-        e.target.lastElementChild.previousElementSibling.addEventListener('click', function() {
-          e.target.setAttribute('contenteditable','false')
-          e.target.lastElementChild.previousElementSibling.innerHTML = '✎'
-          e.target.lastElementChild.style.visibility = 'hidden'
-          e.target.lastElementChild.previousElementSibling.style.visibility = 'hidden'
-          const info = new ProcessText(extract(e.target))
-          postArr.splice(postArr.indexOf(toMatch), 1, info.textEdited())
+        forEdit.lastElementChild.previousElementSibling.addEventListener('click', function() {
+          info.text = extract(forEdit)
+          postArr.splice(postindex, 1, info.textEdited())
           postList.innerHTML = postArr.join(``)
+          console.log(forEdit.childNodes[0])
           formText.focus()
         })
       })
